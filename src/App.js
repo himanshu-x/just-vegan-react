b72385c452ebd4a7d271042a2335bd4ee3a60ddf
himanshu-x <hy12632@gmail.com>
2022-10-19 14:24:22 +0530

    use lazy to reduce bundle size

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { createContext, } from "react";
+import React, { Suspense, lazy, createContext, } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
-import Dishes from "./pages/dishes/dishes";
-import NewDish from "./pages/dishes/new-dish";
-import DishDetails from "./pages/dishes/dish-details";
-import Offer from "./pages/offers/offers";
-import NewOffer from "./pages/offers/new-offer";
-import OfferDetails from "./pages/offers/offer-details";
-import Navbar from "./components/utility-components/Navbar";
-import AuthOptions from "./components/page-components/auth/AuthOptions";
-import Auth from "./pages/auth/auth";
-import Login from "./pages/auth/login";
-import MyAccount from "./pages/my-account/my-account";
-import MyList from "./components/page-components/account/AccountList"
-import Addresses from "./pages/my-account/addresses";
-import AddressDetails from "./pages/my-account/my-address"
-import AddAddress from "./pages/my-account/addAddresses";
-import FavouriteDishes from "./pages/dishes/favourite-dishes";
-import MyOrders from "./pages/my-account/my-order";
-import SignUp from "./pages/auth/sign-up";
-import Home from "./pages/home";
-import DishCartProvider from "./contexts/dish-cart/DishCart.Provider";
 import withBox from "./components/hoc-components/withBox";
-import ProtectedRoutes from "./components/protected-public-admin-routes/ProtectedRoutes";
-import PublicRoutes from "./components/protected-public-admin-routes/PublicRoutes";
 import { getLocalStorage } from "./utils/common.util";
-import AdminRoutes from "./components/protected-public-admin-routes/AdminRoute";
+
+
+const Login = lazy(() => import("./pages/auth/login"));
+const MyAccount = lazy(() => import("./pages/my-account/my-account"));
+const MyList = lazy(() => import("./components/page-components/account/AccountList"));
+const Addresses = lazy(() => import("./pages/my-account/addresses"));
+const AddressDetails = lazy(() => import("./pages/my-account/my-address"));
+const AddAddress = lazy(() => import("./pages/my-account/addAddresses"));
+const FavouriteDishes = lazy(() => import("./pages/dishes/favourite-dishes"));
+const MyOrders = lazy(() => import("./pages/my-account/my-order"));
+const SignUp = lazy(() => import("./pages/auth/sign-up"));
+const Home = lazy(() => import("./pages/home"));
+const DishCartProvider = lazy(() => import("./contexts/dish-cart/DishCart.Provider"));
+const ProtectedRoutes = lazy(() => import("./components/protected-public-admin-routes/ProtectedRoutes"));
+const PublicRoutes = lazy(() => import("./components/protected-public-admin-routes/PublicRoutes"));
+const AdminRoutes = lazy(() => import("./components/protected-public-admin-routes/AdminRoute"));
+const Dishes = lazy(() => import("./pages/dishes/dishes"));
+const NewDish = lazy(() => import("./pages/dishes/new-dish"));
+const DishDetails = lazy(() => import("./pages/dishes/dish-details"));
+const Offers = lazy(() => import("./pages/offers/offers"));
+const NewOffer = lazy(() => import("./pages/offers/new-offer"));
+const Navbar = lazy(() => import("./components/utility-components/Navbar"));
+const AuthOptions = lazy(() => import("./components/page-components/auth/AuthOptions"));
+const Auth = lazy(() => import("./pages/auth/auth"));
+const OfferDetails = lazy(() => import("./pages/offers/offer-details"));
 
 export const UserContext = createContext();
 const MyAccountBox = withBox(MyAccount)
-
-
 
 function App() {
 
@@ -41,37 +41,38 @@ function App() {
 
     <DishCartProvider>
       <Navbar />
-      <Routes>
-
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<ProtectedRoutes />} >
-          <Route path="my-account" element={< MyList />}>
-            <Route index element={<MyAccountBox />} />
-            <Route path="favourite-dishes" element={<FavouriteDishes />}></Route>
-            <Route path="addresses" element={<Addresses />}>
-              <Route path="add-addresses" element={<AddAddress />}></Route>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<ProtectedRoutes />} >
+            <Route path="my-account" element={< MyList />}>
+              <Route index element={<MyAccountBox />} />
+              <Route path="favourite-dishes" element={<FavouriteDishes />}></Route>
+              <Route path="addresses" element={<Addresses />}>
+                <Route path="add-addresses" element={<AddAddress />}></Route>
+              </Route>
+              <Route path="addresses/:address" element={<AddressDetails />}></Route>
+              <Route path="my-orders" element={<MyOrders />}></Route>
             </Route>
-            <Route path="addresses/:address" element={<AddressDetails />}></Route>
-            <Route path="my-orders" element={<MyOrders />}></Route>
           </Route>
-        </Route>
-        <Route path="/" element={<PublicRoutes />}>
-          <Route path="auth" element={<Auth />}>
-            <Route index element={<AuthOptions />} />
-            <Route path="login" element={<Login />}></Route>
-            <Route path="sign-up" element={<SignUp />}></Route>
+          <Route path="/" element={<PublicRoutes />}>
+            <Route path="auth" element={<Auth />}>
+              <Route index element={<AuthOptions />} />
+              <Route path="login" element={<Login />}></Route>
+              <Route path="sign-up" element={<SignUp />}></Route>
+            </Route>
           </Route>
-        </Route>
-        <Route path="/" element={<AdminRoutes />}>
-          <Route path="new-dish" element={<NewDish />} />
-          <Route path="new-offer" element={<NewOffer />} />
-        </Route>
-        <Route path="dishes" element={<Dishes />} ></Route>
-        <Route path="dishes/:dishId" element={<DishDetails />} />
-        <Route path="offers" element={<Offer />} />
-        <Route path="offers/:offerId" element={<OfferDetails />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes >
+          <Route path="/" element={<AdminRoutes />}>
+            <Route path="new-dish" element={<NewDish />} />
+            <Route path="new-offer" element={<NewOffer />} />
+          </Route>
+          <Route path="dishes" element={<Dishes />} ></Route>
+          <Route path="dishes/:dishId" element={<DishDetails />} />
+          <Route path="offers" element={<Offers />} />
+          <Route path="offers/:offerId" element={<OfferDetails />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes >
+      </Suspense>
     </DishCartProvider >
   )
 }
--- src/components/page-components/account/AccountList.jsx
+++ src/components/page-components/account/AccountList.jsx
@@ -12,8 +12,6 @@ export default function AccountOption() {
                 <Link to="/my-account/addresses" ><li className="px-6 py-2 border-b border-gray-200 w-full ">Manage Address</li></Link>
                 <Link to="/my-account/favourite-dishes" ><li className="px-6 py-2 w-full rounded-b-lg">Favourite Dishes</li></Link>
             </ul>
-
-
             <Outlet />
         </div>
     )
